e48b760cfa9df86085feb525f793a754d6d0fc80
filipesanches <filipepereirasanches@gmail.com>
2021-12-28 20:12:31 -0300

    ok

--- aula9.js
+++ aula9.js
@@ -1,5 +1,35 @@
-var idade
-var msg = [`Voce tem ${idade}.`, `Campo idade obrigatorio`]
+var agora = new Date()
+var diaSemana = 98//agora.getDay()
+var idade = 98
+var msg = [`Voce tem ${idade} anos.`, `Campo idade obrigatorio`]
+
+switch (diaSemana) {
+  case 0:
+    console.log('Hoje é Domingo')
+  break;
+  case 1:
+    console.log('Hoje é Segunda')
+  break;
+  case 2:
+    console.log('Hoje é Terça')
+  break;
+  case 3:
+    console.log('Hoje é Quarta')
+  break;
+  case 4:
+    console.log('Hoje é Quinta')
+  break;
+  case 5:
+    console.log('Hoje é Sexta')
+  break;
+  case 6:
+    console.log('Hoje é Sabado')
+  break;
+  default:
+    console.log('[ERRO] - [Ou dia da Semana não existe]')
+  break;
+}
+
 if(idade == null){
   console.log(msg[1])
   console.log('Digite a idade')
