4f15f221ebf2f2e5f4326e368885a5aaf10e7470
filipesanches <filipepereirasanches@gmail.com>
2022-01-01 21:01:18 -0300

    Update aula13.js

--- aula13.js
+++ aula13.js
@@ -1,10 +1,16 @@
-/*var b = 1
-while(b <= 100){
+var a = 1
+while(a <= 100){
   console.log(`Passo ${b}`)
   b++
-}*/
+}
+
 var b = 1
 do {
   console.log(`Passo ${b}`)
   b++  
 } while (b <= 100)
+
+for (let c = 1; c <= 100; c++) {
+  console.log(c)
+  
+}
